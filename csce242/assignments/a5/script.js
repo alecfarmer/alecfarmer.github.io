9e38b3897ad243367b8328fa44e377da02584502
Alec Farmer <realalecfarmer@gmail.com>
2020-02-18 01:10:01 -0500

    Massive update to Main page / a5 and a6

--- csce242/assignments/a5/script.js
+++ csce242/assignments/a5/script.js
@@ -1,23 +1,15 @@
-function displayEmotion() {
-    // Gather all 5 parts of input
-    const firstName = document.getElementById("txt-first-name").value;
-    const lastName = document.getElementById("txt-last-name").value;
-    const productName = document.getElementById("txt-product-name").value;
-    let productCount = document.getElementById("txt-product-count").value;
-    let productPrice = document.getElementById("txt-product-price").value;
-
-    // Math to calculate the total price
-    let pAndC = parseFloat(productPrice)*parseFloat(productCount);
-    let tax = pAndC*parseFloat(0.07);
-    let price = pAndC+tax;
-
-    // Printing the results into html
-    let result = document.getElementById("result");
-    result.innerHTML = `${firstName} ${lastName} ordered ${productCount} ${productName}(s)`;
-    let resultTotal = document.getElementById("result-total");
-    resultTotal.innerHTML = `Totaling: ${price}`
+function toggleSearch() {
+    let searchItems = document.getElementById("search-drop-down");
+    searchItems.classList.toggle("hidden");
 }
 
-// Using a button to call the function
-const btnDisplay = document.getElementById("btn-calculate");
-btnDisplay.onclick = displayEmotion;
+function toggleMenu() {
+    let menuItems = document.getElementById("menu-drop-down")
+    menuItems.classList.toggle("hidden");
+}
+
+const searchToggle = document.getElementById("search");
+searchToggle.onclick = toggleSearch;
+
+const menuToggle = document.getElementById("menu");
+menuToggle.onclick = toggleMenu;
--- csce242/assignments/a6/script.js
+++ csce242/assignments/a6/script.js
@@ -1,23 +1,87 @@
-function displayEmotion() {
-    // Gather all 5 parts of input
+function sortAges() {
+    // Getting data
     const firstName = document.getElementById("txt-first-name").value;
-    const lastName = document.getElementById("txt-last-name").value;
-    const productName = document.getElementById("txt-product-name").value;
-    let productCount = document.getElementById("txt-product-count").value;
-    let productPrice = document.getElementById("txt-product-price").value;
+    const firstAge = document.getElementById("txt-first-age").value;
+    const secondName = document.getElementById("txt-second-name").value;
+    const secondAge = document.getElementById("txt-second-age").value;
+    const thirdName = document.getElementById("txt-third-name").value;
+    const thirdAge = document.getElementById("txt-third-age").value;
 
-    // Math to calculate the total price
-    let pAndC = parseFloat(productPrice)*parseFloat(productCount);
-    let tax = pAndC*parseFloat(0.07);
-    let price = pAndC+tax;
+    let result = document.getElementById("sorted-result");
 
-    // Printing the results into html
-    let result = document.getElementById("result");
-    result.innerHTML = `${firstName} ${lastName} ordered ${productCount} ${productName}(s)`;
-    let resultTotal = document.getElementById("result-total");
-    resultTotal.innerHTML = `Totaling: ${price}`
+    // Checking if input is empty
+    const firstNameError = isBlank(firstName, "error-first-name");
+    const firstAgeError = isBlank(firstAge, "error-first-age");
+    const secondNameError = isBlank(secondName, "error-second-name");
+    const secondAgeError = isBlank(secondAge, "error-second-age");
+    const thirdNameError = isBlank(thirdName, "error-third-name");
+    const thirdAgeError = isBlank(thirdAge, "error-third-age");
+
+    if(firstNameError || firstAgeError || secondNameError || secondAgeError || thirdNameError || thirdAgeError) return;
+
+    // Main function
+    if(firstAge < secondAge) {
+        if(secondAge < thirdAge) {
+            result.innerHTML = `Oldest to youngest: ${thirdName}, ${secondName}, ${firstName}`;
+            console.log(`beep`)
+        } else if(secondAge > thirdAge) {
+            result.innerHTML = `Oldest to youngest: ${secondName}, ${firstName}, ${thirdName}`;
+            console.log(`cheap`)
+        } else {
+            `Oldest to youngest: Invalid Information`;
+        }
+    } else {
+        `Oldest to youngest: Invalid Information`;
+    }
 }
 
-// Using a button to call the function
-const btnDisplay = document.getElementById("btn-calculate");
-btnDisplay.onclick = displayEmotion;
+function fundBar() {
+    // Getting data
+    const fundInput = document.getElementById("txt-fund-total").value;
+    
+    let disTotal = document.getElementById("display-total");
+
+    // Clearing the classes
+    disTotal.classList.remove("third-way", "half-way", "two-thirds-way", "full");
+
+    // Checking if input is empty
+    const firstError = isBlank(fundInput, "error-fund-total");
+
+    if(firstError) return;
+
+    // Main function
+    if(fundInput >= 10000) {
+        disTotal.classList.add("full");
+    } else if(fundInput >=6001) {
+        disTotal.classList.add("two-thirds-way")
+    } else if(fundInput == 6000) {
+        disTotal.classList.add("half-way");
+    } else if(fundInput > 0) {
+        disTotal.classList.add("third-way");
+    }
+}
+
+function isBlank(data, errorSpanId) {
+    if(data.trim() == "") {
+        let errorSpan = document.getElementById(errorSpanId);
+        errorSpan.classList.remove("hidden");
+        return true;
+    } else {
+        let errorSpan = document.getElementById(errorSpanId);
+        errorSpan.classList.add("hidden");
+    }
+}
+
+function toggleNav() {
+    let navItems = document.getElementById("main-nav-items");
+    navItems.classList.toggle("hidden");
+}
+
+const navToggle = document.getElementById("nav-toggle");
+navToggle.onclick = toggleNav;
+
+const ageSort = document.getElementById("btn-compare");
+ageSort.onclick = sortAges;
+
+const fundCalulate = document.getElementById("btn-calculate");
+fundCalulate.onclick = fundBar;
