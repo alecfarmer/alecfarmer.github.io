da77b36e8afaf492a44db83368c7a5a8a45dc3de
Alec Farmer <realalecfarmer@gmail.com>
2020-10-10 21:41:48 -0400

    Yup

--- csce242/assignments/a15/app.js
+++ csce242/assignments/a15/app.js
@@ -134,6 +134,6 @@ function validatePlayer(player) {
     return Joi.validate(player, schema);
 }
 
-app.listen(process.env.PORT || 3000, ()=>{
-    console.log("Listening on port 3000");
+app.listen(process.env.PORT || 3001, ()=>{
+    console.log("Listening on port 3001");
 })
